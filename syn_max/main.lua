5cac3107423e49a4b8587de6465e8bdb0164391c
NeoGaming22 <48037518+NeoGaming22@users.noreply.github.com>
2020-11-13 02:40:51 -0500

    Update

---
+++ syn_max/main.lua
@@ -0,0 +1,13 @@
+Citizen.CreateThread(function()
+    Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, -1845241476, 1084182731, 2, 752097756)
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, 4, 752097756)
+    end
+end)
+
+
